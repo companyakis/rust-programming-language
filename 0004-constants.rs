27352b7dd5f9f9701272cf69b722ede4a1adc83e
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-10-14 21:28:43 +0300

    Create 0004-constants.rs

---
+++ 0004-constants.rs
@@ -0,0 +1,13 @@
+const FIRST_YEAR: u16 = 1923;
+
+fn main() {
+
+    // the type of the value must be annotated!
+
+    // const can be used in the global scope, and let can only be used in a function
+
+    const PI_NUMBER: f32 = 3.14;
+
+    const MY_NAME: &str = "MUSTAFA";
+    
+}
