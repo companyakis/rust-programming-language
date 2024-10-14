dff2b5ff55ac44f094560c934356e468dad5d9cc
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-10-14 21:57:13 +0300

    Create 0008-numeric-operations1.rs

---
+++ 0008-numeric-operations1.rs
@@ -0,0 +1,16 @@
+fn main() {
+
+        let a = 12;
+
+        let b = - 5;
+
+        // addition
+        let sum = a + b;
+
+        // subtraction
+        let difference = a - b;
+    
+        // multiplication
+        let product = a * b;
+  
+}
