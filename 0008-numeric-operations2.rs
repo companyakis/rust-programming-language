01fd6a78c8dae98449c006b7379b697363f00d78
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-10-14 21:58:36 +0300

    Create 0008-numeric-operations2.rs

---
+++ 0008-numeric-operations2.rs
@@ -0,0 +1,25 @@
+fn main() {
+
+        let a = 12;
+
+        let b = - 5;
+    
+        // division
+  
+        let division_1 = a / b;
+
+        println!("Division 1: {division_1}");
+
+        let division_2: f32 = a as f32 / b as f32;
+    
+        println!("Division 2: {division_2}");
+
+        // remainder
+        let remainder = a % b;
+
+        println!("Remainder: {remainder}")
+}
+
+// Division 1: -2
+// Division 1: -2.4
+// Remainder: 2    
