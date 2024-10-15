e2f2a749f06ca1b96f5e2c43a2351e8d59b43c41
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-10-15 08:30:08 +0300

    Create 0013-function3.rs

---
+++ 0013-function3.rs
@@ -0,0 +1,10 @@
+fn main() {
+
+    student_name_birth("Bilge", 2011); // Student name: Bilge and student birth year: 2011
+    
+}
+
+fn student_name_birth(name: &str, year: u16) {
+
+    println!("Student name: {name} and student birth year: {year}")
+}
