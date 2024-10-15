f4340f66d27fc30c01b0b2a9799c3817b5038634
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-10-15 13:35:58 +0300

    Create 0014-if.rs

---
+++ 0014-if.rs
@@ -0,0 +1,16 @@
+fn main() {
+
+  let x= 12;
+
+  let y = -21;
+
+  if x > y {
+    println!("x > y")
+  } 
+  else if y > x {
+    println!("y > x")
+  }
+  else {
+      println!("x == y")
+  }
+}
