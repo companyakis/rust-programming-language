c43f35d735b0a1aa75a838d4075c6e7ce0ec6e95
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-10-15 13:40:07 +0300

    Create 0015-if-bool.rs

---
+++ 0015-if-bool.rs
@@ -0,0 +1,21 @@
+fn main() {
+
+  let x= 12;
+
+  if x > 10 {
+    println!("x > 10")
+  }
+
+  if x != 8 {
+    println!("x is NOT equal to 8!")
+  }
+
+  if x == 12 {
+    println!("x is equal to 12.")
+  }
+
+}
+
+// x > 10
+// x is NOT equal to 8!
+// x is equal to 12.  
