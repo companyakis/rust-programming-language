87fa5715835a08fe6aea60ae12bc8e1da2d91c62
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-10-15 08:20:13 +0300

    Create 0012-array2.rs

---
+++ 0012-array2.rs
@@ -0,0 +1,11 @@
+fn main() {
+
+    let fives = [5; 8];
+
+    println!("Fives: {:?}", fives); // Fives: [5, 5, 5, 5, 5, 5, 5, 5]
+
+    let zeros: [u8; 12] = [0; 12];
+
+    println!("Zeros: {:?}", zeros); // Zeros: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+}
