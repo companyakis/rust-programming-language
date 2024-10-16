ec040a11bde318833ab0515961627feb428ef892
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-10-16 13:05:19 +0300

    Create 0019-for4.rs

---
+++ 0019-for4.rs
@@ -0,0 +1,24 @@
+fn main() {
+
+  for number in (-3..=10).rev() {
+
+    println!("Reversed Doubled: {}", number * 2)
+
+  }
+
+}
+
+// Reversed Doubled: 20
+// Reversed Doubled: 18
+// Reversed Doubled: 16
+// Reversed Doubled: 14
+// Reversed Doubled: 12
+// Reversed Doubled: 10
+// Reversed Doubled: 8 
+// Reversed Doubled: 6 
+// Reversed Doubled: 4 
+// Reversed Doubled: 2 
+// Reversed Doubled: 0 
+// Reversed Doubled: -2
+// Reversed Doubled: -4
+// Reversed Doubled: -6
