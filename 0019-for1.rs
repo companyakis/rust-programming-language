71bbd072b24ebf2bf9897a05f59e2a41992b6292
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-10-16 12:48:55 +0300

    Create 0019-for1.rs

---
+++ 0019-for1.rs
@@ -0,0 +1,16 @@
+fn main() {
+
+  let years = [2018, 2021, 2023, 2024, 2026];
+
+  for year in years {
+    
+    println!("Year: {year}")
+  }
+
+}
+
+// Year: 2018
+// Year: 2021
+// Year: 2023
+// Year: 2024
+// Year: 2026
