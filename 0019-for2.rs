4e0d7ebce4d7c109637c734d0a1f38ff45f0a679
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-10-16 12:57:01 +0300

    Create 0019-for2.rs

---
+++ 0019-for2.rs
@@ -0,0 +1,23 @@
+fn main() {
+
+  for number in -3..10 {
+
+    println!("Doubled: {}", number * 2)
+
+  }
+
+}
+
+// Doubled: -6
+// Doubled: -4
+// Doubled: -2
+// Doubled: 0 
+// Doubled: 2 
+// Doubled: 4 
+// Doubled: 6 
+// Doubled: 8 
+// Doubled: 10
+// Doubled: 12
+// Doubled: 14
+// Doubled: 16
+// Doubled: 18
