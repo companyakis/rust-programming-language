f3685884f4a29823bfc0b0b44e0c331d9ba3b16a
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-10-16 12:45:53 +0300

    Create 0018-while.rs

---
+++ 0018-while.rs
@@ -0,0 +1,16 @@
+fn main() {
+
+  let mut counter: i8 = 9;
+
+  while counter != -4 {
+    
+    print!("{counter} ");
+
+    counter -= 1
+  }
+
+  print!("...")
+
+}
+
+// 9 8 7 6 5 4 3 2 1 0 -1 -2 -3 ...
