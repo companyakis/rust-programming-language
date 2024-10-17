b748828b775f28a8c510e670f06f2db82f3b1817
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-10-17 09:03:05 +0300

    Create 0021-enum1.rs

---
+++ 0021-enum1.rs
@@ -0,0 +1,17 @@
+fn main() {
+
+  let capital: Cities = Cities::Ankara;
+
+  let most_popular_city = Cities::Istanbul;
+
+}
+
+enum Cities {
+  Ankara,
+  Istanbul,
+  Izmir,
+  Adana,
+  Kahramanmaras,
+  Kayseri
+}
+
