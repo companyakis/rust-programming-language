41e827441a051d2703048d759d8b2785b657eb2c
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-10-17 11:13:34 +0300

    Create 0023-string2.rs

---
+++ 0023-string2.rs
@@ -0,0 +1,17 @@
+fn main() {
+
+  // updating
+
+  let mut name = String::new();
+
+  name.push_str("Mustafa");
+
+  println!("My name is {}!", name); // My name is Mustafa!
+
+  name.push_str(" Büyükdereli");
+
+  println!("My name is {}!", name); // My name is Mustafa Büyükdereli!
+
+}
+
+
