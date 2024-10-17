d161c1f09db9cef30dc5507f7866e615d7635376
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-10-17 11:10:04 +0300

    Create 0023-string1.rs

---
+++ 0023-string1.rs
@@ -0,0 +1,15 @@
+fn main() {
+
+  // empty but mutable
+
+  let mut proverb = String::new();
+
+  println!("Proverb: {}", proverb); // Proverb: 
+
+  proverb = "Out of sight, out of mind".to_string();
+
+  println!("Proverb: {}", proverb); // Proverb: Out of sight, out of mind
+
+}
+
+
