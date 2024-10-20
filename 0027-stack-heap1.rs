b8b6bbd724e926ffe6342d877832a3a0785c071e
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-10-20 23:17:01 +0300

    Create 0027-stack-heap1.rs

---
+++ 0027-stack-heap1.rs
@@ -0,0 +1,18 @@
+fn main() {
+
+  let year: u16 = 2024; // stored on the stack
+
+  let my_name = String::from("Mustafa");
+
+  // move
+
+  let a_name = my_name;
+
+  // error!!!
+
+  // println!("My name is {my_name}"); // value borrowed here after move
+
+  println!("Whose name is {a_name}?") // Whose name is Mustafa?
+
+}
+
